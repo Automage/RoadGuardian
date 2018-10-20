dde180dde57caebfeff33225d601ca88a858d895
Automage <pranav92000@gmail.com>
2018-10-20 17:54:16 -0400

    Script to combine individual records of car crashes into a .csv containing the sum of crashes in a certain day and hour

---
+++ venv/RoadGuardian.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/Pranav/Documents/Hackathons/HackHarvard/RoadGuardian/venv/test2.csv')
+numRows = len(df.index);
+prevDate = df.iloc[1, 0]
+prevHour = df.iloc[1, 1]
+dateList = []
+hourList = []
+finalHourList = []
+accidentsList = []
+hourCount = 1
+
+for x in range (numRows):
+    date = df.iloc[x, 0]
+    print(date)
+    hour = df.iloc[x, 1]
+    print(hour)
+    if date == prevDate:
+        if hour == prevHour:
+            hourCount = hourCount + 1
+            print ("hourCount: "+str(hourCount))
+        else:
+            dateList.append(prevDate)
+            hourList.append(prevHour)
+            accidentsList.append(hourCount)
+            hourCount = 1
+            prevHour = hour
+            prevDate = date
+    else:
+        dateList.append(prevDate)
+        prevDate = date
+        #finalHourList.append(prevHour)
+        #hourList.clear()
+        hourList.append(prevHour)
+        prevHour = hour
+        accidentsList.append(hourCount)
+        hourCount = 1
+
+dateList.append(prevDate)
+hourList.append(prevHour)
+#hourList.append(prevHour)
+#finalHourList = finalHourList + hourList
+#finalHourList.append(prevHour)
+#print(finalHourList)
+#print("Added to final")
+accidentsList.append(hourCount)
+
+print("date list: "+str(len(dateList)))
+print("hour list: "+str(len(hourList)))
+#print("final hour list: "+str(len(finalHourList)))
+
+dictionary = {"date": dateList, "hour": hourList, "accidents": accidentsList}
+out = pd.DataFrame(dictionary)
+out.to_csv('out.csv')
